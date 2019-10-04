19eb2105bf2651007380d4730dd4052b81ffc486
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-10-04 05:20:48 -0400

    added return

--- search_class.py
+++ search_class.py
@@ -32,14 +32,15 @@ class Search:
         results = bio.search(query, count)
         id_list = results['IdList']
         papers = bio.fetch_details(id_list)
+        title = []
+        abstract = []
         for i, paper in enumerate(papers['PubmedArticle']): 
-            title = "%d) %s" % (i+1, paper['MedlineCitation']['Article']['ArticleTitle'])
-            abstract = "%d) %s" % (i+1, paper['MedlineCitation']['Article']['Abstract']['AbstractText'])
-            return title, abstract
+            T = "%d) %s" % (i+1, paper['MedlineCitation']['Article']['ArticleTitle'])
+            A = "%d) %s" % (i+1, paper['MedlineCitation']['Article']['Abstract']['AbstractText'])
+            title.append(T)
+            abstract.append(A)
+        return title, abstract
             
 
-if __name__ == '__main__':
-    bio = Search()
 
-    bio.query("MMP-13 inhibitors", '5')
     
