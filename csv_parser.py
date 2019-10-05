0efca4cc958a2ab32658c0172221a8d800d21cdd
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-10-05 12:44:33 -0400

    added csv parser

--- csv_parser.py
+++ csv_parser.py
@@ -4,7 +4,7 @@ import re
 class Csv_Parser:
     def __init__(self):
         self.val = 6
-    def string_split(self, string):
+    def find_keyword(self, string):
         key_sentence = []
         for i in string:
             string = ', '.join(i[0])
@@ -18,7 +18,8 @@ class Csv_Parser:
 if __name__ == '__main__':
     bio = Search()
     csv = Csv_Parser()
-    title, abstract = bio.query("MMP-13 inhibitors", '4')
-    l = csv.string_split(abstract)
-    for i in l:
+    title, abstract = bio.query("MMP-13 inhibitors", '10')
+    
+    sentence = csv.find_keyword(abstract)
+    for i in sentence:
         print(i)
